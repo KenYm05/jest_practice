188efa8b9c7f92303a3014a16ca6ad795245627a
DESKTOP-ESG8K2K\yaman <k.yamanoue19920519@gmail.com>
2022-01-21 11:44:08 +0900

    サンプル実装

--- app/test/index.test.js
+++ app/test/index.test.js
@@ -2,19 +2,15 @@ import {default as MedicalExamination} from "../src/index";
 import {Person} from "../src/person";
 
 test('利用人数が3人であるか', () => {
-    const medical_examination = new MedicalExamination();
-    expect(medical_examination.getNumberOfPeople()).toHaveLength(3);
+
 });
 
 test('年齢が20歳であるか', () => {
-    const medical_examination = new MedicalExamination();
-    expect(medical_examination.isAdult()).toBeTruthy();
+
 });
 
 test('利用者の中に佐藤がいるか', () => {
-    const medical_examination = new MedicalExamination();
-    const expected = ["佐藤"];
-    expect(medical_examination.getNumberOfPeople()).toEqual(expect.arrayContaining(expected));
+
 });
 
 test('BMIを測定し、指数が普通体重以下が返ってくることを確認', () => {
